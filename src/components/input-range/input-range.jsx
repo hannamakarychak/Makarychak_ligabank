af8faa1893904197d2e8ed1ded31f20601f9182a
hannamakarychak <annamakarichak3@gmail.com>
2021-10-02 12:07:11 +0200

    fix logic in inout-range and numeric-input

--- src/components/input-range/input-range.jsx
+++ src/components/input-range/input-range.jsx
@@ -1,28 +1,11 @@
 import classNames from 'classnames';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import { SliderInput, SliderTrack, SliderRange, SliderHandle } from '@reach/slider';
 
 import '@reach/slider/styles.css';
 import './input-range.scss';
 
-const InputRange = ({ value, totalValue, className, onChange }) => {
-  const [percentage, setPercentage] = useState(10);
-
-  useEffect(() => {
-    setPercentage(10);
-  }, [totalValue]);
-
-  const handleChangePercentage = (newPercentage) => {
-    setPercentage(newPercentage);
-    onChange((newPercentage / 100) * totalValue);
-  };
-
-  const handleChangeInitialPayment = (e) => {
-    const newInitialPayment = parseInt(e.target.value, 10);
-    setPercentage((newInitialPayment / totalValue) * 100);
-    onChange(newInitialPayment);
-  };
-
+const InputRange = ({ value, percentage, className, onChange, onPercentageChange }) => {
   return (
     <Fragment>
       <div className={classNames(className, 'input-range')}>
@@ -30,11 +13,11 @@ const InputRange = ({ value, totalValue, className, onChange }) => {
           type="number"
           className="input-range__field"
           value={value}
-          onChange={handleChangeInitialPayment}
+          onChange={(e) => onChange(parseInt(e.target.value, 10))}
         />
         <span className="input-range__text">рублей</span>
       </div>
-      <SliderInput min={10} max={100} step={5} onChange={handleChangePercentage} value={percentage}>
+      <SliderInput min={10} max={100} step={5} onChange={onPercentageChange} value={percentage}>
         <SliderTrack>
           <SliderRange />
           <SliderHandle />
--- src/components/numeric-input/numeric-input.jsx
+++ src/components/numeric-input/numeric-input.jsx
@@ -2,16 +2,7 @@ import classNames from 'classnames';
 
 import './numeric-input.scss';
 
-// const INVALID_PRICE = 'Некорректное значение';
-
 const NumericInput = ({ value, className, onChange }) => {
-  // const [isInvalid, setIsInvalid] = useState(false);
-
-  // useEffect(() => {
-  //   const newPrice = parseInt(price, 10);
-  //   setIsInvalid(isNaN(newPrice) || newPrice < 1200000 || newPrice > 25000000);
-  // }, [price]);
-
   return (
     <div className={classNames(className, 'numeric-input')}>
       <button onClick={() => onChange(value - 100000)} className="numeric-input__button">
