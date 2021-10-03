fbaa5ca89b6fcacace6f6032f7e205e4084718ff
hannamakarychak <annamakarichak3@gmail.com>
2021-10-03 18:55:42 +0200

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
 import Hero from './components/hero/hero';
